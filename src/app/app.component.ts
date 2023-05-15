79278e8c8b372f58dd254917954af0f3774dec79
Sourav Nayak <souravnk1199@gmail.com>
2023-05-15 21:31:29 +0530

    just commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { WikipediaService } from './wikipedia.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'wsearch';
+  pages:any = [];
+
+  constructor(private wikipedia: WikipediaService) {}
+
+  onTerm(term: string) {
+    this.wikipedia.search(term).subscribe((pages) => {
+      this.pages = pages;
+      console.log(this.pages);
+    });
+  }
 }
